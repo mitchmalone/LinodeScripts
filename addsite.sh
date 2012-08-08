48268333b4c62910a6c44683586efecaeeba608f
Mitch Malone <mitch.malone@me.com>
2012-08-08 21:26:14 +1000

    Updated permissions stuff

--- addsite.sh
+++ addsite.sh
@@ -6,17 +6,17 @@ touch /home/mitch/www/$1/logs/access.log
 echo "$1 coming soon" > /home/mitch/www/$1/public_html/index.html
 
 # 2. Create the Apache config
-echo "<VirtualHost *:80>" > /etc/apache2/sites-available/$1
-echo "    ServerAdmin mitch.malone@me.com" >> /etc/apache2/sites-available/$1
-echo "    ServerName $1" >> /etc/apache2/sites-available/$1
-echo "    ServerAlias www.$1" >> /etc/apache2/sites-available/$1
-echo "    DocumentRoot /home/mitch/www/$1/public_html/" >> /etc/apache2/sites-available/$1
-echo "    ErrorLog /home/mitch/www/$1/logs/error.log" >> /etc/apache2/sites-available/$1
-echo "    CustomLog /home/mitch/www/$1/logs/access.log combined" >> /etc/apache2/sites-available/$1
-echo "</VirtualHost>" >> /etc/apache2/sites-available/$1
+sudo echo "<VirtualHost *:80>
+>    ServerAdmin mitch.malone@me.com
+>    ServerName $1
+>    ServerAlias www.$1
+>    DocumentRoot /home/mitch/www/$1/public_html/
+>    ErrorLog /home/mitch/www/$1/logs/error.log
+>    CustomLog /home/mitch/www/$1/logs/access.log combined
+> </VirtualHost>" > /etc/apache2/sites-available/$1
 
 # 3. Add site
 a2ensite $1
 
 # 4. Restart Apache
-/etc/init.d/apache2 reload
+sudo /etc/init.d/apache2 reload
